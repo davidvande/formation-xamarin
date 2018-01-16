76142fc352bac50b666e9f09cd92d7a53982b9b0
David VANDE <david.vande@gmail.com>
2018-01-16 09:29:08 +0100

    Gitignore le lab

---
+++ projets/leLabComputer/leLabComputer/Views/HomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace leLabComputer.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            this.Title = "Sommaire";
+        }
+
+        async void btCompteur_Clicked(Object sender, EventArgs e)
+        {
+            var countPage = new CountPage();
+            await Navigation.PushAsync(countPage);
+        }
+    }
+}
