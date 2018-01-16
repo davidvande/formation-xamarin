1921f17c55a1b46b17ba816ddbc535f62563b01e
David VANDE <david.vande@gmail.com>
2018-01-16 10:52:26 +0100

    Test layout + doc

---
+++ projets/layoutTests/layoutTests/Views/RelativeLayoutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace layoutTests.Views
+{
+    public partial class RelativeLayoutPage : ContentPage
+    {
+        public RelativeLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
