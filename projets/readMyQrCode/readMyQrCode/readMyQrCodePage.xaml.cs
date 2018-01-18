00b9f636aed8ebfd17a9aeae675d96caffded002
David VANDE <david.vande@gmail.com>
2018-01-18 17:00:12 +0100

    Doc + début qrcode

---
+++ projets/readMyQrCode/readMyQrCode/readMyQrCodePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+namespace readMyQrCode
+{
+    public partial class readMyQrCodePage : ContentPage
+    {
+        public readMyQrCodePage()
+        {
+            InitializeComponent();
+        }
+
+        async protected void btScan_Clicked(Object sender, EventArgs e)
+        {
+            #if __ANDROID__
+                // Initialize the scanner first so it can track the current context
+                MobileBarcodeScanner.Initialize(Application);
+            #endif
+            
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+            var result = await scanner.Scan();
+
+            if (result != null)
+                Console.WriteLine("Scanned Barcode: " + result.Text);
+        }
+    }
+}
