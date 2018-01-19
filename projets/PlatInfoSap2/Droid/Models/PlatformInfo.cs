ee3de361fb24c11729052e084c789de4f1e41d2a
David VANDE <david.vande@gmail.com>
2018-01-19 10:17:12 +0100

    SAP avec class spe

---
+++ projets/PlatInfoSap2/Droid/Models/PlatformInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.OS;
+
+namespace PlatInfoSap2.Models
+{
+    public class PlatformInfo
+    {
+        public string GetModel()
+        {
+            return String.Format("{0} {1}", Build.Manufacturer,
+                                            Build.Model);
+        }
+
+        public string GetVersion()
+        {
+            return Build.VERSION.Release.ToString();
+        }
+    }
+}
